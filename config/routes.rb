17e0a2c0ccbd3723607aca6d8014ff0c7b76a151
afsheen minnotte <afsheenminnotte@gmail.com>
2023-10-07 17:35:36 +0400

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+#   # Rails.application.routes.draw do
+#   get "restaurants", to: "restaurants#index"
+#   get "restaurants/new", to: "restaurants#new"
+#   post "restaurants", to: "restaurants#create"
+#   get "restaurants/:id", to: "restaurants#show"
+# end
+Rails.application.routes.draw do
+  resources :tasks
 end
